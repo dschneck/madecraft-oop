c0724bfd096b62afae30143ca3463da21cc9a05f
David Schneck <david.schneckster@hotmail.com>
2020-10-06 01:19:40 -0400

    Finished Section 3 excersises

--- 3. Getters and Setters/exercises/Minion.java
+++ 3. Getters and Setters/exercises/Minion.java
@@ -1,5 +1,38 @@
 package exercises;
 
 public class Minion {
+    private String name; 
+    private int eyes; 
+    private String color; 
+    private String master;
 
+    // Constructor
+    Minion(String name, int eyes, String color, String master) {
+        this.name = name;
+        this.eyes = eyes;
+        this.color = color;
+        this.master = master;
+        
+    }
+
+    // Getters
+    public String getName() {
+        return this.name;
+    }
+    public String getMaster() {
+        return this.master;
+    }
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getEyes() {
+        return this.eyes;
+    }
+
+    // Setters
+    public void setMaster(String master) { 
+        this.master = master;
+    }
+    
 }
--- 3. Getters and Setters/exercises/MinionTest.java
+++ 3. Getters and Setters/exercises/MinionTest.java
@@ -1,10 +1,9 @@
 package exercises;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
+/* *
  * To make these tests pass, you will need to create a Minion class with the member variables below.
  * 
  * <code>
@@ -16,33 +15,34 @@ import org.junit.Test;
  * 
  * Create a constructor, and getters and setters for the member variables. If they’re done right, these tests will pass.
  * 
- * **/
+ * * */
 
 public class MinionTest {
+	/*
+	@Test
+	public void testConstructor() {
+		Minion stuart = new Minion("Stuart", 1, "yellow", "");
+		assertEquals("Stuart", stuart.getName());
+		assertEquals(1, stuart.getEyes());
+		assertEquals("yellow", stuart.getColor());
 
-//	@Test
-//	public void testConstructor() {
-//		Minion stuart = new Minion("Stuart", 1, "yellow", "");
-//		assertEquals("Stuart", stuart.getName());
-//		assertEquals(1, stuart.getEyes());
-//		assertEquals("yellow", stuart.getColor());
-//
-//		Minion dave = new Minion("Dave", 2, "yellow", "");
-//		assertEquals("Dave", dave.getName());
-//		assertEquals(2, dave.getEyes());
-//		assertEquals("yellow", dave.getColor());
-//	}
-//
-//	@Test
-//	public void testSetters() {
-//		Minion stuart = new Minion("Stuart", 1, "yellow", "");
-//
-//		stuart.setMaster("T. Rex");
-//		assertEquals("T. Rex", stuart.getMaster());
-//
-//		stuart.setMaster("Napoleon");
-//		assertEquals("Napoleon", stuart.getMaster());
-//	}
+		Minion dave = new Minion("Dave", 2, "yellow", "");
+		assertEquals("Dave", dave.getName());
+		assertEquals(2, dave.getEyes());
+		assertEquals("yellow", dave.getColor());
+	}
+*/
+
+	@Test
+	public void testSetters() {
+		Minion stuart = new Minion("Stuart", 1, "yellow", "");
+
+		stuart.setMaster("T. Rex");
+		assertEquals("T. Rex", stuart.getMaster());
+
+		stuart.setMaster("Napoleon");
+		assertEquals("Napoleon", stuart.getMaster());
+	}
 
 }
 
